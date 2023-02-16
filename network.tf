fbbb3ee4d3d358a982fcb1038930922300d6f4ad
root <root@ip-172-31-1-174.ec2.internal>
2023-02-16 00:07:49 +0000

    Add: Create subnets priv and pub

--- network.tf
+++ network.tf
@@ -6,6 +6,53 @@ resource "aws_vpc" "vorx-vpc-prod" {
   }
 }
 
+
+resource "aws_subnet" "vorx-subnet-pub-1a" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public-Subnet-1a"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-pub-1b" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Public-Subnet-1b"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1a" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private-Subnet-1a"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1b" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.20.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private-Subnet-1b"
+  }
+}
+
+
+
+
+
+
+
 output "vpc_vorx_prod_id" {
   value = aws_vpc.vorx-vpc-prod.id
 }
@@ -13,3 +60,26 @@ output "vpc_vorx_prod_id" {
 output "vpc_vorx_prod_arn" {
   value = aws_vpc.vorx-vpc-prod.arn
 }
+
+output "vorx_prod_subnet_pub-1a" {
+  value = aws_subnet.vorx-subnet-pub-1a.id
+}
+
+
+output "vorx_prod_subnet_priv-1a" {
+  value = aws_subnet.vorx-subnet-priv-1a.id
+}
+
+output "vorx_prod_subnet_pub-1b" {
+  value = aws_subnet.vorx-subnet-pub-1b.id
+}
+
+
+output "vorx_prod_subnet_priv-1b" {
+  value = aws_subnet.vorx-subnet-priv-1b.id
+}
+
+
+
+
+
